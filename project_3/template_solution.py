2ad11803ddb27bdc1daa368481271be71ea5224a
ChrisPirker <chrispirker1@gmail.com>
2026-04-27 20:36:26 +0200

    Convoluted Neural Networks attempt 5

--- project_3/template_solution.py
+++ project_3/template_solution.py
@@ -6,242 +6,145 @@ import matplotlib.pyplot as plt
 import torch
 from torch.utils.data import DataLoader, TensorDataset
 import torch.nn as nn
-import torch.nn.functional as F
 
-"""
-README FIRST
-
-The below code is a template for the solution. You can change the code according
-to your preferences, but the testing function has to save the output of your 
-model on the test data as it does in this template. This output must be submitted.
-
-Replace the dummy code with your own code in the TODO sections.
-
-We also encourage you to use tensorboard or wandb to log the training process
-and the performance of your model. This will help you to debug your model and
-to understand how it is performing. But the template does not include this
-functionality.
-Link for wandb:
-https://docs.wandb.ai/quickstart/
-Link for tensorboard: 
-https://pytorch.org/tutorials/recipes/recipes/tensorboard_with_pytorch.html
-"""
-
-# The device is automatically set to GPU if available, otherwise CPU
-# If you want to force the device to CPU, you can change the line to
-# device = torch.device("cpu")
-
-# If you have a Mac consult the following link:
-# https://pytorch.org/docs/stable/notes/mps.html
-
-# It is important that your model and all data are on the same device.
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
-def load_data(**kwargs):
-    """
-    Get the training and test data. The data files are assumed to be in the
-    same directory as this script.
-
-    Args:
-    - kwargs: Additional arguments that you might find useful - not necessary
-
-    Returns:
-    - train_data_input: Tensor[N_train_samples, C, H, W]
-    - train_data_label: Tensor[N_train_samples, C, H, W]
-    - test_data_input: Tensor[N_test_samples, C, H, W]
-    where N_train_samples is the number of training samples, N_test_samples is
-    the number of test samples, C is the number of channels (1 for grayscale),
-    H is the height of the image, and W is the width of the image.
-    """
-    # Load the training data
+def load_data():
     train_data = np.load("train_data.npz")["data"]
-
-    # Make the training data a tensor
-    train_data = torch.tensor(train_data, dtype=torch.float32)
-
-    # Load the test data
     test_data_input = np.load("test_data.npz")["data"]
 
-    # Make the test data a tensor
-    test_data_input = torch.tensor(test_data_input, dtype=torch.float32)
+    train_data = torch.tensor(train_data, dtype=torch.float32) / 255.0
+    test_data_input = torch.tensor(test_data_input, dtype=torch.float32) / 255.0
 
-    ########################################
     train_data_label = train_data.clone()
     train_data_input = train_data.clone()
 
-    # From task description: Given an image of dimensions (28, 28) we set the center 8x8 pixels to black, i.e. mask them
-    # train_data_input: torch.Size([60000, 1, 28, 28])
     train_data_input[:, :, 10:18, 10:18] = 0
-
-    # Visualize the training data if needed
-    # Set to False if you don't want to save the images
-    if True:
-        # Create the output directory if it doesn't exist
-        if not Path("train_image_output").exists():
-            Path("train_image_output").mkdir()
-        for i in tqdm(range(20), desc="Plotting train images"):
-            # Show the training and the target image side by side
-            plt.subplot(1, 2, 1)
-            plt.imshow(train_data_input[i].squeeze(), cmap="gray")
-            plt.title("Training Input")
-            plt.subplot(1, 2, 2)
-            plt.title("Training Label")
-            plt.imshow(train_data_label[i].squeeze(), cmap="gray")
-
-            plt.savefig(f"train_image_output/image_{i}.png")
-            plt.close()
 
     return train_data_input, train_data_label, test_data_input
 
 
-def training(train_data_input, train_data_label, **kwargs):
-    """
-    Train the model. Fill in the details of the data loader, the loss function,
-    the optimizer, and the training loop.
+class Model(nn.Module):
+    def __init__(self):
+        super().__init__()
 
-    Args:
-    - train_data_input: Tensor[N_train_samples, C, H, W]
-    - train_data_label: Tensor[N_train_samples, C, H, W]
-    - kwargs: Additional arguments that you might find useful - not necessary
+        self.net = nn.Sequential(
+            nn.Conv2d(1, 32, 3, padding=1),
+            nn.LeakyReLU(0.1),
 
-    Returns:
-    - model: torch.nn.Module
-    """
-    model = Model()     # Initialize the model (class: Model, subclass of torch.nn.Module).
-    model.train()       # Set the module in training mode. Switch off with model.eval().
-    model.to(device)    # Sends model to device (GPU/CPU).
+            nn.Conv2d(32, 64, 3, padding=1),
+            nn.LeakyReLU(0.1),
 
-    criterion = torch.nn.MSELoss()                              # Loss function.
-    optimizer = torch.optim.SGD(model.parameters(), lr=1e-4)    # Optimizer.
+            nn.Conv2d(64, 64, 3, padding=1),
+            nn.LeakyReLU(0.1),
 
-    batch_size = 32                                                         # Batch size.
-    dataset = TensorDataset(train_data_input, train_data_label)             # Dataset. tuple: (x, y), x: train features, y: train labels. TensorDataset: Dataset wrapping tensors.
-    data_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)  # DataLoader. class, iterable: ((x, y), (x, y), ...). Splits the dataset into 1875 batches of size 32. Shuffle: reshuffles the data at every epoch.
+            nn.Conv2d(64, 32, 3, padding=1),
+            nn.LeakyReLU(0.1),
 
-    n_epochs = 30
+            nn.Conv2d(32, 1, 3, padding=1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        pred = self.net(x)
+
+        out = x.clone()
+        out[:, :, 10:18, 10:18] = pred[:, :, 10:18, 10:18]
+
+        return out
+
+
+def training(train_data_input, train_data_label):
+    model = Model().to(device)
+    model.train()
+
+    criterion = nn.MSELoss()
+
+    optimizer = torch.optim.AdamW(
+        model.parameters(),
+        lr=1e-4,
+        weight_decay=1e-6
+    )
+
+    batch_size = 256
+    dataset = TensorDataset(train_data_input, train_data_label)
+    data_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+
+    n_epochs = 10
+
     for epoch in range(n_epochs):
-        for x, y in tqdm(
-            data_loader, desc=f"Training Epoch {epoch}", leave=False
-        ): # tqdm() shows a progress bar. Loop: n=batch_size gradient descent steps per epoch.
-            x, y = x.to(device), y.to(device)
+        running_loss = 0.0
+
+        for x, y in tqdm(data_loader, desc=f"Training Epoch {epoch}", leave=False):
+            x = x.to(device)
+            y = y.to(device)
+
             optimizer.zero_grad()
+
             output = model(x)
-            loss = criterion(output, y)
+
+            loss = criterion(
+                output[:, :, 10:18, 10:18],
+                y[:, :, 10:18, 10:18]
+            )
+
             loss.backward()
+
+            torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
+
             optimizer.step()
 
-        print(f"Epoch {epoch} loss: {loss.item()}")
+            running_loss += loss.item()
+
+        avg_loss = running_loss / len(data_loader)
+        print(f"Epoch {epoch} loss: {avg_loss:.6f}")
 
     return model
 
 
-# TODO: define a model. Here, a basic MLP model (fully connected NN) is defined. You can completely
-# change this model - and are encouraged to do so.
-class Model(nn.Module): # subclass of PyTorch class "Module" https://docs.pytorch.org/docs/stable/generated/torch.nn.Module.html
-    """
-    Implement your model here.
-    """
-
-    def __init__(self):
-        """
-        The constructor of the model.
-        """
-        super().__init__()
-        self.fc = nn.Sequential(
-            nn.Linear(784, 784),
-            nn.ReLU(),
-            nn.Linear(784, 784),
-            nn.ReLU(),
-            nn.Linear(784, 784),
-            nn.ReLU()
-        )
-
-    def forward(self, x):
-        """
-        The forward pass of the model.
-
-        input: x: torch.Tensor, the input to the model
-
-        output: x: torch.Tensor, the output of the model
-        """
-        # Flatten the image in the last two dimensions
-        x = x.view(x.shape[0], -1)
-        x = self.fc(x)
-        # Reshape the image to the original shape
-        x = x.view(x.shape[0], 1, 28, 28)
-        return x
-
-
 def testing(model, test_data_input):
-    """
-    Uses your model to predict the ouputs for the test data. Saves the outputs
-    as a binary file. This file needs to be submitted. This function does not
-    need to be modified except for setting the batch_size value. If you choose
-    to modify it otherwise, please ensure that the generating and saving of the
-    output data is not modified.
-
-    Args:
-    - model: torch.nn.Module
-    - test_data_input: Tensor
-    """
     model.eval()
     model.to(device)
 
     with torch.no_grad():
         test_data_input = test_data_input.to(device)
-        # Predict the output batch-wise to avoid memory issues
+
         test_data_output = []
-        # TODO: You can increase or decrease this batch size depending on your
-        # memory requirements of your computer / model
-        # This will not affect the performance of the model and your score
-        batch_size = 32
+        batch_size = 128
+
         for i in tqdm(
             range(0, test_data_input.shape[0], batch_size),
             desc="Predicting test output",
         ):
-            output = model(test_data_input[i : i + batch_size])
+            output = model(test_data_input[i:i + batch_size])
             test_data_output.append(output.cpu())
+
         test_data_output = torch.cat(test_data_output)
 
-    # Ensure the output has the correct shape
-    assert test_data_output.shape == test_data_input.shape, (
-        f"Expected shape {test_data_input.shape}, but got "
-        f"{test_data_output.shape}."
-        "Please ensure the output has the correct shape."
-        "Without the correct shape, the submission cannot be evaluated and "
-        "will hence not be valid."
-    )
+    assert test_data_output.shape == test_data_input.shape
 
-    # Save the output
-    test_data_output = test_data_output.numpy()
-    # Ensure all values are in the range [0, 255]
+    test_data_output = test_data_output.numpy() * 255.0
+
     save_data_clipped = np.clip(test_data_output, 0, 255)
-    # Convert to uint8
-    # Ensure your model outputs values in the [0, 255] range before this step! If you normalized your data to [0, 1], you must multiply by 255 before saving.
     save_data_uint8 = save_data_clipped.astype(np.uint8)
-    # Loss is only computed on the masked area - so set the rest to 0 to save
-    # space
+
     save_data = np.zeros_like(save_data_uint8)
     save_data[:, :, 10:18, 10:18] = save_data_uint8[:, :, 10:18, 10:18]
 
-    np.savez_compressed(
-        "submit_this_test_data_output.npz", data=save_data)
+    np.savez_compressed("submit_this_test_data_output.npz", data=save_data)
 
-    # You can plot the output if you want
-    # Set to False if you don't want to save the images
     if True:
-        # Create the output directory if it doesn't exist
-        if not Path("test_image_output").exists():
-            Path("test_image_output").mkdir()
+        Path("test_image_output").mkdir(exist_ok=True)
+
         for i in tqdm(range(20), desc="Plotting test images"):
-            # Show the training and the target image side by side
             plt.subplot(1, 2, 1)
             plt.title("Test Input")
             plt.imshow(test_data_input[i].squeeze().cpu().numpy(), cmap="gray")
+
             plt.subplot(1, 2, 2)
-            plt.imshow(test_data_output[i].squeeze(), cmap="gray")
             plt.title("Test Output")
+            plt.imshow(test_data_output[i].squeeze(), cmap="gray")
 
             plt.savefig(f"test_image_output/image_{i}.png")
             plt.close()
@@ -249,22 +152,17 @@ def testing(model, test_data_input):
 
 def main():
     seed = 0
-    # Reproducibility
     torch.manual_seed(seed)
     np.random.seed(seed)
-    torch.backends.cudnn.deterministic = True
 
-    # You don't need to change the code below
-    # Load the data
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(seed)
+
     train_data_input, train_data_label, test_data_input = load_data()
-    # Train the model
+
     model = training(train_data_input, train_data_label)
 
-    # Test the model (this also generates the submission file)
-    # The name of the submission file is submit_this_test_data_output.npz
     testing(model, test_data_input)
-
-    return None
 
 
 if __name__ == "__main__":
